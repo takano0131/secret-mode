349336b56c224f3c69ad98a9dbdec416014d1464
m.takano.ryouta@gmail.com <m.takano.ryouta@gmail.com>
2024-06-07 09:43:04 +0900

    retry

--- index.js
+++ index.js
@@ -1,66 +1,62 @@
-function isIncognito(callback) {
-  const fs = window.RequestFileSystem || window.webkitRequestFileSystem;
-  if (!fs) {
-      console.log('ブラウザが対応していません。');
-      callback(false);
+function detectIncognitoMode(callback) {
+  const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  
+  if (isChrome) {
+      const fs = window.RequestFileSystem || window.webkitRequestFileSystem;
+      if (!fs) {
+          callback(false); // 旧バージョンのChrome
+      } else {
+          fs(window.TEMPORARY, 100, () => callback(false), () => callback(true));
+      }
   } else {
-      fs(window.TEMPORARY, 100, () => callback(false), () => callback(true));
+      // 他のブラウザ用の検出ロジック
+      detectNonChromeIncognitoMode(callback);
   }
 }
 
-function checkPrivateBrowsing() {
-  return new Promise((resolve) => {
-      const on = () => resolve(true); // シークレットモード
-      const off = () => resolve(false); // 通常モード
+function detectNonChromeIncognitoMode(callback) {
+  const on = () => callback(true); // シークレットモード
+  const off = () => callback(false); // 通常モード
 
-      const testLocalStorage = () => {
+  const testLocalStorage = () => {
+      try {
+          if (window.localStorage) {
+              window.localStorage.setItem('test', '1');
+              window.localStorage.removeItem('test');
+              off();
+          } else {
+              on();
+          }
+      } catch (e) {
+          on();
+      }
+  };
+
+  const ua = navigator.userAgent;
+  const isSafari = /Safari/i.test(ua) && !/Chrome/i.test(ua);
+  const isMobile = /Mobile|Android|iP(ad|hone)/i.test(ua);
+
+  if (isSafari) {
+      let test = () => {
           try {
-              if (window.localStorage) {
-                  window.localStorage.setItem('test', '1');
-                  window.localStorage.removeItem('test');
-                  off();
-              } else {
-                  on();
-              }
+              window.openDatabase(null, null, null, null);
+              off();
           } catch (e) {
               on();
           }
       };
-
-      const ua = navigator.userAgent;
-      const isSafari = /Safari/i.test(ua) && !/Chrome/i.test(ua);
-      const isMobile = /Mobile|Android|iP(ad|hone)/i.test(ua);
-
-      if (isSafari) {
-          let test = () => {
-              try {
-                  window.openDatabase(null, null, null, null);
-                  off();
-              } catch (e) {
-                  on();
-              }
-          };
-          if (isMobile) {
-              setTimeout(test, 100); // モバイルSafariのために少し待つ
-          } else {
-              test();
-          }
+      if (isMobile) {
+          setTimeout(test, 100); // モバイルSafariのために少し待つ
       } else {
-          testLocalStorage();
+          test();
       }
-  });
+  } else {
+      testLocalStorage();
+  }
 }
 
-isIncognito((isInIncognito) => {
+detectIncognitoMode((isInIncognito) => {
   if (isInIncognito) {
-      console.log('シークレットモードです。');
-  } else {
-      console.log('通常モードです。');
-  }
-});
-
-checkPrivateBrowsing().then((isPrivate) => {
-  if (isPrivate) {
       console.log('シークレットモードです。');
   } else {
       console.log('通常モードです。');
